359edb1c46605636727567b9d03d37a333852d57
Jes Andersen <sindal@gmail.com>
2010-11-17 00:47:05 +0100

    Added dragsource interface

---
+++ src/com/kk_electronic/kkportal/core/dnd/DragSource.java
@@ -0,0 +1,7 @@
+package com.kk_electronic.kkportal.core.dnd;
+
+import com.google.gwt.event.dom.client.HasMouseDownHandlers;
+
+public interface DragSource {
+	HasMouseDownHandlers getDragHandle();
+}
