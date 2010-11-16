47bc3857cc2a2065ca3a29d33b30963408b3f163
Jes Andersen <sindal@gmail.com>
2010-11-16 16:06:17 +0100

    Removed unnecessary import

--- server/w2b/debughelp.py
+++ server/w2b/debughelp.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with KKPortal.  If not, see <http://www.gnu.org/licenses/>.
 #
-from __future__ import print_function
-
 from os import path
 
 def run():
