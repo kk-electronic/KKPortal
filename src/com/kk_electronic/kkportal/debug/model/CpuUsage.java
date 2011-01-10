1ad08ba338d684aa1662ab3b32a4919b004ccdf4
Jes Andersen <sindal@gmail.com>
2011-01-10 12:29:47 +0100

    Class commenting, and variable rename

--- src/com/kk_electronic/kkportal/debug/model/CpuUsage.java
+++ src/com/kk_electronic/kkportal/debug/model/CpuUsage.java
@@ -10,9 +10,19 @@ import com.google.gwt.view.client.HasData;
 import com.google.inject.Inject;
 import com.kk_electronic.kkportal.core.services.TechDemo;
 
+/**
+ * I hold the data for Cpu Usage on the server, and manage a list
+ * of any class that is interested in data.
+ * I listen for any {@link NewCpuUsageDataEvent} and update my data
+ * accordingly.
+ * Whenever I update my data I make sure to call setRowData on any
+ * display that is connected to this data. 
+ * @author Jes Andersen
+ *
+ */
 public class CpuUsage implements AsyncCallback<List<Double>>,
 		NewCpuUsageDataEvent.Handler {
-	List<Double> l = new LinkedList<Double>();
+	List<Double> cachedData = new LinkedList<Double>();
 	List<HasData<Double>> displays = new LinkedList<HasData<Double>>();
 
 	@Inject
@@ -42,18 +52,25 @@ public class CpuUsage implements AsyncCallback<List<Double>>,
 
 	@Override
 	public void onSuccess(List<Double> result) {
-		l = result;
-		updateDisplays(l);
+		cachedData = result;
+		updateDisplays(cachedData);
 	}
 
 	@Override
 	public void onNewCpuUsage(NewCpuUsageDataEvent event) {
+		/*
+		 * We get data for all cpus in the system and a single
+		 * combined one.
+		 * The names are "cpu" for the total and "cpuX" where
+		 * X is a zero based index of the cpu/core
+		 * The class only takes care of the combined data currently.
+		 */
 		if ("cpu".equals(event.getCpuname())) {
-			l.add(event.getCpuload());
-			if(l.size()>60){
-				l.remove(0);
+			cachedData.add(event.getCpuload());
+			if(cachedData.size()>60){
+				cachedData.remove(0);
 			}
-			updateDisplays(l);
+			updateDisplays(cachedData);
 		}
 	}
 }
