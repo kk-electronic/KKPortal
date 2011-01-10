d20ba0fc1d54c877b3b7a4fcaea475964b6fa1f9
Jes Andersen <sindal@gmail.com>
2011-01-10 12:30:34 +0100

    Class cleanup and commenting of UsageGraph

--- src/com/kk_electronic/kkportal/debug/modules/UsageGraph.java
+++ src/com/kk_electronic/kkportal/debug/modules/UsageGraph.java
@@ -21,10 +21,18 @@ import com.google.inject.Inject;
 import com.kk_electronic.kkportal.core.AbstractModule;
 import com.kk_electronic.kkportal.debug.model.CpuUsage;
 
+/**
+ * This Module provides a smooth graph by animating the steps between the data points.
+ * We use a HTML5 Canvas to display the values using a simple path with no interpolation.
+ * @author Jes Andersen
+ */
 public class UsageGraph extends AbstractModule implements HasData<Double>{
 	GWTCanvas canvas = new GWTCanvas();
 	private List<Double> values;
 	
+	/*
+	 * This class was used for testing before the back-end was implemented
+	 */
 	public static class RandomWalk{
 		List<Double> l = new LinkedList<Double>();
 		List<HasData<Double>> displays = new LinkedList<HasData<Double>>();
@@ -60,6 +68,9 @@ public class UsageGraph extends AbstractModule implements HasData<Double>{
 		}
 	}
 	
+	/*
+	 * This timer calls drawPath every time it is triggered.
+	 */
 	Timer timer = new Timer() {
 		
 		@Override
@@ -69,6 +80,11 @@ public class UsageGraph extends AbstractModule implements HasData<Double>{
 	};
 	private double offset;
 
+	/*
+	 * The constructor has two responsibilities.
+	 * Add the module as a display of the CpuUsage
+	 * Start the timer that updates graph
+	 */
 	@Inject
 	public UsageGraph(CpuUsage model) {
 		model.addDisplay(this);
@@ -88,17 +104,20 @@ public class UsageGraph extends AbstractModule implements HasData<Double>{
 	    timer.scheduleRepeating(100);
 	}
 	
+	/*
+	 * We use the IsWidget interface rather than inheriting from Widget since the latter
+	 * would be expensive in unit tests of the class
+	 * @see com.google.gwt.user.client.ui.IsWidget#asWidget()
+	 */
 	@Override
 	public Widget asWidget() {
 		return canvas;
 	}
 
-	@Override
-	public SelectionModel<? super Double> getSelectionModel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/*
+	 * When the model updates the data it calls setRow Data
+	 * @see com.google.gwt.view.client.HasData#setRowData(int, java.util.List)
+	 */
 	@Override
 	public void setRowData(int start, List<Double> values) {
 		
@@ -106,91 +125,96 @@ public class UsageGraph extends AbstractModule implements HasData<Double>{
 		this.values = values;
 	}
 
+	/*
+	 * This functions draws the data on the canvas with a potential shift to the left. It will
+	 * slowly move the data points to the left while we are waiting for new data creating a smooth
+	 * update of the graph.
+	 * The constructor will ensure this function gets called every 0.1 seconds
+	 */
 	private void drawPath(List<Double> values, double shift) {
 		if(values == null || values.isEmpty()) return;
 		canvas.clear();
 	    canvas.beginPath();
 	    double pixelpersecond = canvas.getCoordWidth()/57.0; 
 	    int o = Math.max(60-values.size(),0); // How many missing values
-	    canvas.moveTo((o-shift-1)*pixelpersecond, canvas.getCoordHeight()*values.get(0));
-//	    canvas.moveTo((1+o)*pixelpersecond, canvas.getCoordHeight()*values.get(0));
+	    canvas.moveTo((o-shift*2-1)*pixelpersecond, canvas.getCoordHeight()*values.get(0));
+//	    canvas.moveTo((o)*pixelpersecond, canvas.getCoordHeight()*values.get(0));
 	    for(int i=1,l=values.size();i<l;i++){
-			double x = (i+o-shift-1)*pixelpersecond;
-//	    	double x = (1+i+o)*pixelpersecond;
+			double x = (i+o-shift*2-1)*pixelpersecond;
+//	    	double x = (i+o)*pixelpersecond;
 			double y = canvas.getCoordHeight()*(1-values.get(i));
 			canvas.lineTo(x,y);
 		}
 		canvas.stroke();
 	}
 
+	/*
+	 * All the functions below are null implementations needed the the GWT HasData interface
+	 * We should perhaps use a more simple interface for simple data models. 
+	 * @see com.google.gwt.view.client.HasData#getSelectionModel()
+	 */
+	@Override
+	public SelectionModel<? super Double> getSelectionModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 	@Override
 	public void setSelectionModel(SelectionModel<? super Double> selectionModel) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void setVisibleRangeAndClearData(Range range,
 			boolean forceRangeChangeEvent) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public HandlerRegistration addRangeChangeHandler(Handler handler) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	@Override
 	public HandlerRegistration addRowCountChangeHandler(
 			com.google.gwt.view.client.RowCountChangeEvent.Handler handler) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 	@Override
 	public Range getVisibleRange() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	@Override
 	public boolean isRowCountExact() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 	@Override
 	public void setRowCount(int count) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void setRowCount(int count, boolean isExact) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void setVisibleRange(Range range) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void setVisibleRange(int start, int length) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void fireEvent(GwtEvent<?> event) {
 		// TODO Auto-generated method stub
