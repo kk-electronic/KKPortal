d9ff2703965f97bb58699089431eee03e90bc4e9
Jes Andersen <sindal@gmail.com>
2011-01-10 12:32:12 +0100

    Made the login screen display message of the day if available.

--- src/com/kk_electronic/kkportal/core/security/IdentityProvider.java
+++ src/com/kk_electronic/kkportal/core/security/IdentityProvider.java
@@ -5,28 +5,68 @@ import java.util.List;
 
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.kk_electronic.kkportal.core.ui.InputDialog;
 import com.kk_electronic.kkportal.core.ui.InputDialog.Handler;
+import com.kk_electronic.kkportal.examples.modules.MotDService;
 
 @Singleton
 public class IdentityProvider implements Handler{
 	private final InputDialog display;
 	private final EventBus eventBus;
+	private final String dialogtext = "Username:";
+	private String motd;
 	
 	public static interface Display {
 		void show();
+		void isShowing();
 		void setPresenter(IdentityProvider identityProvider);
 	}
 
 	@Inject
-	public IdentityProvider(InputDialog display,EventBus eventBus) {
+	public IdentityProvider(final InputDialog display,EventBus eventBus,MotDService motDService) {
 		this.display = display;
 		this.eventBus = eventBus;
+		motDService.getMessageOfTheDay(new AsyncCallback<String>() {
+			
+			@Override
+			public void onSuccess(String result) {
+				setMotd(result);
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				setMotd(null);
+			}
+		});
 		display.setHandler(this);
 	}
 	
+	protected void setMotd(String newmotd) {
+		if(newmotd == null || newmotd.endsWith("\n")){
+			motd = newmotd;			
+		} else {
+			motd = newmotd + "\n";
+		}
+		updateText();
+	}
+
+	private void updateText() {
+		if(display.isShowing()){
+			display.setText(getDialogText());
+		}
+	}
+
+	private String getDialogText() {
+		if(motd == null){
+			return dialogtext;
+		} else {
+			return motd + dialogtext;
+		}
+	}
+
 	List<Identity> identities = new LinkedList<Identity>();
 	public void invalidate(Identity identity){
 		identities.remove(identity);
@@ -37,6 +77,7 @@ public class IdentityProvider implements Handler{
 	public Identity getPrimaryIdentity(){
 		if(identities.size() > 0) return identities.get(0);
 		display.show();
+		display.setText(getDialogText());
 		return null;
 	}
 
