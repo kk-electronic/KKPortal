4949c0e053ce3079ec70f7dca100f6a2de2c225b
Jes Andersen <sindal@gmail.com>
2011-01-14 01:14:08 +0100

    added equals and identity to hashcode

--- src/com/kk_electronic/kkportal/core/security/Identity.java
+++ src/com/kk_electronic/kkportal/core/security/Identity.java
@@ -1,6 +1,36 @@
 package com.kk_electronic.kkportal.core.security;
 
 public class Identity {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((server == null) ? 0 : server.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Identity other = (Identity) obj;
+		if (server == null) {
+			if (other.server != null)
+				return false;
+		} else if (!server.equals(other.server))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 	public String getUsername() {
 		return username;
 	}
