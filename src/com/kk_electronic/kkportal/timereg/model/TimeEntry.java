97dd534277c38d0257a0a2d61ab31193e7924ffc
Jes Andersen <sindal@gmail.com>
2011-02-15 12:05:34 +0100

    Added databean

---
+++ src/com/kk_electronic/kkportal/timereg/model/TimeEntry.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010 kk-electronic a/s. 
+ * 
+ * This file is part of KKPortal.
+ *
+ * KKPortal is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KKPortal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with KKPortal.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.kk_electronic.kkportal.timereg.model;
+
+import java.util.Date;
+
+
+/**
+ * @author albatros
+ *
+ */
+public class TimeEntry {
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getCheckin() {
+		return checkin;
+	}
+	public void setCheckin(Date checkin) {
+		this.checkin = checkin;
+	}
+	public Date getCheckout() {
+		return checkout;
+	}
+	public void setCheckout(Date checkout) {
+		this.checkout = checkout;
+	}
+	public Integer getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(Integer taskId) {
+		this.taskId = taskId;
+	}
+	public TimeEntry(Date checkin,Date checkout, String string) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	private Integer id;
+	private Date checkin;
+	private Date checkout;
+	private Integer taskId;
+}
