617af9df25613ca1c6a767fc1a868e9b603bbffe
Jes Andersen <sindal@gmail.com>
2011-02-15 12:02:43 +0100

    Added new range class

---
+++ src/com/kk_electronic/kkportal/core/util/Range.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010 kk-electronic a/s. 
+ * 
+ * This file is part of KKPortal.
+ *
+ * KKPortal is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KKPortal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with KKPortal.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.kk_electronic.kkportal.core.util;
+
+/**
+ * @author Jes Andersen
+ * 
+ */
+public class Range<T extends Comparable<T>> implements Cloneable {
+	public T begin;
+	public T end;
+
+	public Range() {
+	}
+
+	public Range(T begin, T end) {
+		this.begin = begin;
+		this.end = end;
+	}
+
+	public boolean isBounded() {
+		return begin != null && end != null;
+	}
+	
+	public boolean contains(Range<T> range){
+		if (range.begin.compareTo(begin) < 0){
+			return false;
+		}
+		if (range.end.compareTo(end) > 0){
+			return false;
+		}
+		return true;
+	}
+
+	public Range<T> clone() {
+		return new Range<T>(begin, end);
+	}
+}
