d38468fec2dde7e19dd51c0abf8b79a2769692a5
Jes Andersen <sindal@gmail.com>
2011-04-15 16:56:59 +0200

    Added JsonBoolean and deleted unused JsonTable

--- src/com/kk_electronic/kkportal/core/rpc/jsonformat/JsonBoolean.java
+++ src/com/kk_electronic/kkportal/core/rpc/jsonformat/JsonBoolean.java
@@ -21,24 +21,26 @@ package com.kk_electronic.kkportal.core.rpc.jsonformat;
 
 import java.util.List;
 
+import com.google.gwt.json.client.JSONBoolean;
 import com.google.gwt.json.client.JSONValue;
 import com.kk_electronic.kkportal.core.rpc.FrameEncoder;
-import com.kk_electronic.kkportal.core.rpc.Table;
 
-public class JsonTable<T> implements JsonValue<Table<T>> {
+public class JsonBoolean implements JsonValue<Boolean> {
 
-	@Override
-	public Table<T> fromJson(JSONValue jsonValue, List<Class<?>> subtypes,
-			FrameEncoder<JSONValue> encoder) throws UnableToDeserialize {
-		// TODO Auto-generated method stub
-		return null;
+	public JsonBoolean() {
 	}
 
 	@Override
-	public void toJson(StringBuilder response, Table<T> object,
+	public void toJson(StringBuilder response, Boolean object,
 			FrameEncoder<JSONValue> encoder) throws UnableToSerialize {
-		// TODO Auto-generated method stub
-		
+		response.append(object.toString());
 	}
 
+	@Override
+	public Boolean fromJson(JSONValue jsonValue, List<Class<?>> subtypes,
+			FrameEncoder<JSONValue> encoder) throws UnableToDeserialize {
+		JSONBoolean number = jsonValue.isBoolean();
+		if(number == null) throw new UnableToDeserialize("Expected json number");
+		return number.booleanValue();
+	}
 }
